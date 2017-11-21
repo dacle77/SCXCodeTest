8cdc3f789a83c7d7eda4284e2528e76419aa5397
dacle77 <cwaustin@mac.com>
2017-11-21 12:37:17 -0800

    added files

--- SCXCodeTestOne/ViewController.swift
+++ SCXCodeTestOne/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Source Control through XCode 
+        // Source Control through XCode
     }
 
     override func didReceiveMemoryWarning() {
