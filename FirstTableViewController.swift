673c5c4a431d723a5a8738633e41a9ec0f91f8eb
Cleland Austin <cwaustin@mac.com>
2017-11-21 12:54:28 -0800

    added new file from portable

---
+++ FirstTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FirstTableViewController.swift
+//  SCXCodeTestOne
+//
+//  Created by dacle on 11/21/17.
+//  Copyright © 2017 Cleland Austin. All rights reserved.
+//
+
+import UIKit
+
+class FirstTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //
+        // Added new tableView
+        //
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
